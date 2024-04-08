f285bd15a52df4ab622ca88b4d5f777ae55fc60b
Danielle Smal <2312053@students.wits.ac.za>
2024-04-08 14:22:55 +0000

    add console before retruning file

--- api/getallcars/index.js
+++ api/getallcars/index.js
@@ -8,7 +8,8 @@ app.use(express.json());
 const cars = require('./src/cars.json');
 
 module.exports = async function (context, req) {
-    res.json(cars);
     console.log("Called Cars")
+    console.log(cars)
+    res.json(cars);
     console.log("Found it!")
 };
