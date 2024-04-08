27d74bd826646205fcd3db2096627188420ac0fc
Danielle Smal <2312053@students.wits.ac.za>
2024-04-08 14:05:30 +0000

    change to azure api

---
+++ api/getallcars/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+
+
+app.use(express.json());
+
+const cars = require('./cars.json');
+
+module.exports = async function (context, req) {
+    context.res.json({
+        cars
+    });
+    console.log("Found it!")
+};
